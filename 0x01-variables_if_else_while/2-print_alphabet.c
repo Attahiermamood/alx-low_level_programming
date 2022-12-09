20caca5a0fe4059e9a9a88d2faab3cabf2d92198
attahermamood <attahermahmood@gmail.com>
2022-12-09 11:06:22 -0800

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ ** main - main block
+ ** Description: prints the alphabet in lowercase
+ ** Return: 0
+ **/
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+		putchar('\n');
+		return (0);
+}
