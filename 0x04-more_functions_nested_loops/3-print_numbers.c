a4d6c6a54d17f4b4ba7cfa55b11bb78e44dbdf38
attahermamood <attahermahmood@gmail.com>
2022-12-15 11:08:37 -0800

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - prints the numbers, from 0 to 9
+ * Return: 0
+ */
+void print_numbers(void)
+{
+	char c = 0;
+
+	for (c >= 48; c <= 57; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
