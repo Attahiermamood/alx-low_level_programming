2131a45636f880a4997c0c5985dab4234b1cb1eb
attahermamood <attahermahmood@gmail.com>
2022-12-15 11:27:10 -0800

    task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_most_numbers - prints the numbers, from 0 to 9
+ * Return: 0
+ */
+void print_most_numbers(void)
+{
+	char c = 0;
+
+	while (c <= 9)
+	{
+		while (c != 2 && c != 4)
+		{
+			_putchar('0' + c);
+		}
+		c++;
+	}
+	_putchar('\n');
+}
